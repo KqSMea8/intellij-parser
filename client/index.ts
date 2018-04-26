62ce933ac6cdcaecc495442e4b5c19cfeec747f2
linfeng <bensheng.xbs@alibaba-inc.com>
2018-04-26 10:46:10 +0800

    fix tslint

--- client/index.ts
+++ client/index.ts
@@ -1,16 +1,14 @@
 /** 用例 */
-import SQLWorker from './SQLWorker.ts';
+import SQLWorker from './SQLWorker';
 
 var worker = new SQLWorker({
   severity: 'error'
-})
+});
 
-for(let i=0;i<5;i++) {
+for (let i = 0; i < 5; i++) {
   setTimeout(() => {
     worker.doHover('select * from a', 31).then(data => {
-      console.log(data)
-    })
-  }, 1)
+      console.log(data);
+    });
+  }, 1);
 }
-
-
